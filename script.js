3bdaa57bbe16a2dc2a0d6ec9c5687b93169f4089
unknown <danielgregoire34@gmail.com>
2023-03-18 22:24:43 -0400

    added in the rest of the forcast data, and used w3 css to style the application

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ var textSearch = document.getElementById('textSearch').value;
         let currentDate = `${weatherMonth}/${weatherDay}/${weatherYear}`;
         $('p.date').text(''+currentDate);
         var currentTemp = response.main.temp;
-        $('p.current-temp').text('Temp: ' + currentTemp + ' Degrees Fahrenheit');
+        $('p.current-temp').text('Temp: ' + currentTemp + ' °F');
         var currentWind = response.wind.speed; 
-        $('p.current-wind').text('Wind: ' + currentWind + 'mph');
+        $('p.current-wind').text('Wind: ' + currentWind + ' MPH');
         var currentHumidity = response.main.humidity;
         $('p.current-humidity').text('Humidity: ' + currentHumidity + '%');
         var iconCode = response.weather[0].icon;
@@ -46,6 +46,10 @@ var textSearch = document.getElementById('textSearch').value;
         .then(function(response){
             console.log(response)
             forcastOne(response);
+            forcastTwo(response);
+            forcastThree(response);
+            forcastFour(response);
+            forcastFive(response);
         })
 
 
@@ -62,14 +66,72 @@ function forcastOne(response)
     var hours = response.list[0].dt_txt.substring(0, 10);
     $('p#hours2').text('Date: ' + hours);
     var temperature = response.list[0].main.temp_max;
-    $('p#temp2').text('Temp: ' + temperature + ' Degrees Fahrenheit');
+    $('p#temp2').text('Temp: ' + temperature + ' °F');
     var winds = response.list[0].wind.speed;
-    $('p#wind2').text('Wind: ' + winds + 'mph');
-    var humid = response.list[0].main.humid;
-    $('p#humid2').text('Humidity: ' + humid + '%');
-
+    $('p#wind2').text('Wind: ' + winds + ' MPH');
+    var humids = response.list[0].main.humidity;
+    $('p#humid2').text('Humidity: ' + humids + '%');
 }
 
+function forcastTwo(response)
+{
+    var icon = response.list[8].weather[0].icon;
+    var icon = 'http://openweathermap.org/img/w/' + icon + ".png";
+    $('img.icon3').attr('src', icon);
+    var hours = response.list[8].dt_txt.substring(0, 10);
+    $('p#hours3').text('Date: ' + hours);
+    var temperature = response.list[8].main.temp_max;
+    $('p#temp3').text('Temp: ' + temperature + ' °F');
+    var winds = response.list[8].wind.speed;
+    $('p#wind3').text('Wind: ' + winds + ' MPH');
+    var humids = response.list[8].main.humidity;
+    $('p#humid3').text('Humidity: ' + humids + '%');
+}
+
+function forcastThree(response)
+{
+    var icon = response.list[16].weather[0].icon;
+    var icon = 'http://openweathermap.org/img/w/' + icon + ".png";
+    $('img.icon4').attr('src', icon);
+    var hours = response.list[16].dt_txt.substring(0, 10);
+    $('p#hours4').text('Date: ' + hours);
+    var temperature = response.list[16].main.temp_max;
+    $('p#temp4').text('Temp: ' + temperature + ' °F');
+    var winds = response.list[16].wind.speed;
+    $('p#wind4').text('Wind: ' + winds + ' MPH');
+    var humids = response.list[16].main.humidity;
+    $('p#humid4').text('Humidity: ' + humids + '%');
+}
+
+function forcastFour(response)
+{
+    var icon = response.list[24].weather[0].icon;
+    var icon = 'http://openweathermap.org/img/w/' + icon + ".png";
+    $('img.icon5').attr('src', icon);
+    var hours = response.list[24].dt_txt.substring(0, 10);
+    $('p#hours5').text('Date: ' + hours);
+    var temperature = response.list[24].main.temp_max;
+    $('p#temp5').text('Temp: ' + temperature + ' °F');
+    var winds = response.list[24].wind.speed;
+    $('p#wind5').text('Wind: ' + winds + ' MPH');
+    var humids = response.list[24].main.humidity;
+    $('p#humid5').text('Humidity: ' + humids + '%');
+}
+
+function forcastFive(response)
+{
+    var icon = response.list[32].weather[0].icon;
+    var icon = 'http://openweathermap.org/img/w/' + icon + ".png";
+    $('img.icon6').attr('src', icon);
+    var hours = response.list[32].dt_txt.substring(0, 10);
+    $('p#hours6').text('Date: ' + hours);
+    var temperature = response.list[32].main.temp_max;
+    $('p#temp6').text('Temp: ' + temperature + ' °F');
+    var winds = response.list[32].wind.speed;
+    $('p#wind6').text('Wind: ' + winds + ' MPH');
+    var humids = response.list[32].main.humidity;
+    $('p#humid6').text('Humidity: ' + humids + '%');
+}
 
 
 function searchHistory()
